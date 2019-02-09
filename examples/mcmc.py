420657b92fdf0a87958033f265e869e46e7ee6c4
Kris Pickrell <kpickrell@xogrp.com>
2019-02-09 15:58:51 -0600

    * Implement metropolis algorithm for mcmc

---
+++ examples/mcmc.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+
+def _main():
+    obs = np.array( [ 100, 85, 92 ] )
+    prior = norm(np.mean(obs),np.std(obs))
+    samples = np.zeros(10000)
+    samples[0] = 105
+    for i in range(1,len(samples)):
+        proposal = samples[i-1] + np.random.randn()*5
+        if prior.pdf(proposal) / prior.pdf(samples[i-1]) > np.random.uniform():
+            samples[i] = proposal
+        else:
+            samples[i] = samples[i-1]
+
+    sns.distplot(samples, bins=200)
+    plt.show()
+
+    return 0
+
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit( _main() )
